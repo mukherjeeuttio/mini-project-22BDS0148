ff1318bb8a58cf629f1649b9ed982f316d2da13a
Uttio Mukherjee <mukherjeeuttio@gmail.com>
2024-03-28 19:43:45 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>This is a dummy React app.</p>
+    </div>
+  );
+}
+
+export default App;
