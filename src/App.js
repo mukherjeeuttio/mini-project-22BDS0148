22e5a718b623a8a1fd976f860bed308bfe8809f6
Uttio Mukherjee <mukherjeeuttio@gmail.com>
2024-04-10 01:30:13 +0530

    Day 1 ends

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
-
-
+import {Footer, Blog, Possibility, Features, Header, WhatGPT3} from './containers';
+import {CTA, Brand, Navbar} from './components';
+import "./App.css";
 const App = () => {
-  return (
-    <div>
-      <h1>GPT-3</h1>
-    </div>
-  )
+    return (
+        <div>
+                <div className="App">
+                        <div className="gradient__bg">
+                                <Navbar />
+                                <Header />
+                        </div>
+                        <Brand />
+                        <WhatGPT3 />
+                        <Features />
+                        <Possibility />
+                        <CTA />
+                        <Blog />
+                        <Footer />
+                </div> 
+        </div> 
+    )
 }
 
 export default App
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <div>
-      
+      <h1>Navbar</h1>
     </div>
   )
 }
